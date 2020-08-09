9cbf804e7c49a06a5b5262b9192503b428f04213
Rozi Ghazaryan <roza.ghazaryan.9@gmail.com>
2020-08-09 22:27:11 +0400

    first commit

---
+++ src/components/test/test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.scss";
+
+function Test() {
+  return (
+      <div className="testtt">safsdfsdf</div>
+  );
+}
+
+export default Test;
---
+++ src/modules/homePage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.scss";
+import Test from "../../components/test/test";
+
+function HomePage() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default HomePage;
