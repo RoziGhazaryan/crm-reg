b61f9e00a81d44b2d5a3d3d38e21519018fa0613
Rozi <roza.ghazaryan.9@gmail.com>
2020-08-13 17:14:46 +0400

    Register page done

---
+++ src/modules/register/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+import Header from "../../components/header/Header";
+import RegisterC from "../../components/register/register";
+
+function Register() {
+  return (
+    <div>
+        <Header />
+        <RegisterC />    
+    </div>
+  );
+}
+
+export default Register;
